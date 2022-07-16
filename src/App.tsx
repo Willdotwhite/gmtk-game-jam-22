e3ad7571da2a743804ff8af0082c0e5c867b3c2c
William White <william.white@skybettingandgaming.com>
2022-07-16 15:09:15 +0100

    Starting layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import {DiceCollection} from "./components/DiceCollection";
 import {DiceState} from "./DiceState";
 
+const columnStyles = "h-full inline-block"
+
 function App() {
   const [diceState, setDiceState] = useState(new DiceState())
 
   return (
-    <div className="bg-black">
-        <div className="w-1/2">
+    <>
+        <div className={`${columnStyles} w-3/12 bg-slate-400`}>
             <DiceCollection diceState={diceState} />
         </div>
-        <div className="w-1/2">Right(ish)</div>
-    </div>
+        <div className={`${columnStyles} w-9/12 bg-slate-200`}>
+            Right(ish)
+        </div>
+    </>
   )
 }
 
